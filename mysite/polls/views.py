3bf21728f81db4dd26afe31c312ce2cf658c46c4
ybekele <yonael@ualberta.ca>
2019-01-30 18:09:57 -0700

    question 8:

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 
 from django.shortcuts import get_object_or_404, render
 
+from django.urls import reverse
 from .models import Question
 # Create your views here.
 def index(request):
@@ -15,8 +16,8 @@ def detail(request, question_id):
     return render(request, 'polls/detail.html', {'question': question})
 
 def results(request, question_id):
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', {'question': question})
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
